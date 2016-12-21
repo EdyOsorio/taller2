18c9179442c6906f90af17c3289e356adf43eebc
EDX\santy <santy_g33@hotmail.com>
2016-12-21 13:36:35 -0500

    Taller(EdisonOsorio)..21-12-2016

---
+++ Cuadrado.py
@@ -0,0 +1,15 @@
+import turtle
+t= turtle.Pen()
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.reset()
+
+for x in range(1,6):
+    t.forward(100)
+    t.left(216)
+turtle.getscreen()._root.mainloop()
